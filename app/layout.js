7bbb96418bfe8f4bee6e2f9f4d6d08eae6ebc69b
Sahil Ahmed <sahilahmedtcg@gmail.com>
2025-06-13 10:02:23 +0530

    Data added

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -15,7 +16,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      baseTheme:dark
+    }}>
       <html lang="en" suppressHydrationWarning>
         <body
           className={`${inter.className}`}
@@ -28,9 +31,9 @@ export default function RootLayout({ children }) {
           >
             {/* header */}
             <Header />
-            <main>{children}</main>
+            <main className="min-h-screen">{children}</main>
             {/* footer */}
-            <footer className="bg-muted/50  py-12">
+            <footer className="bg-muted/50 py-12 mt-10">
               <div className="container mx-auto px-4 text-center text-gray-200">
                 <p>Made with ❤️ by Sahil</p>
               </div>
--- components/header.jsx
+++ components/header.jsx
@@ -48,49 +48,57 @@ const Header = () => {
                 <span className="hidden md:block ">Industry Insights</span>
               </Button>
             </Link>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Button variant="default">
+                  <StarsIcon className="h-4 w-4" />
+                  <span className="hidden md:block ">Growth Tools</span>
+                  <ChevronDown className="h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>
+                  <Link href={"/resume"} className="flex items-center gap-2">
+                    <FileText className="h-4 w-4" />
+                    <span>Build Resume</span>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link
+                    href={"/ai-cover-letter"}
+                    className="flex items-center gap-2"
+                  >
+                    <PenBox className="h-4 w-4" />
+                    <span>Cover Letter</span>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href={"/interview"} className="flex items-center gap-2">
+                    <GraduationCap className="h-4 w-4" />
+                    <span>Interview Prep</span>
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </SignedIn>
-
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Button variant="default">
-                <StarsIcon className="h-4 w-4" />
-                <span className="hidden md:block ">Growth Tools</span>
-                <ChevronDown className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuItem>
-                <Link href={"/resume"} className="flex items-center gap-2">
-                  <FileText className="h-4 w-4" />
-                  <span>Build Resume</span>
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Link
-                  href={"/ai-cover-letter"}
-                  className="flex items-center gap-2"
-                >
-                  <PenBox className="h-4 w-4" />
-                  <span>Cover Letter</span>
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Link href={"/interview"} className="flex items-center gap-2">
-                  <GraduationCap className="h-4 w-4" />
-                  <span>Interview Prep</span>
-                </Link>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-
           <SignedOut>
             <SignInButton>
-                <Button variant="outline">Sign In</Button>
+              <Button variant="outline">Sign In</Button>
             </SignInButton>
             <SignUpButton />
           </SignedOut>
           <SignedIn>
-            <UserButton />
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                  userButtonPopoverCard: "shadow-xl",
+                  userPreviewMainIdentifier: "font-semibold",
+                },
+              }}
+              afterSignOutUrl="/"
+            />
           </SignedIn>
         </div>
       </nav>
